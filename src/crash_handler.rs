9e6ed2d8c4b95b7c351022ce67f72bc163cad6e3
zocomputer <help@zocomputer.com>
2026-09-09 07:47:55 +0000

    crash diagnostics: write() takes c_uint on Windows

--- src/crash_handler.rs
+++ src/crash_handler.rs
@@ -48,7 +48,7 @@ pub fn append_to_log(msg: &str) {
                 libc::write(
                     fd,
                     line.as_ptr().add(written) as *const libc::c_void,
-                    (line.len() - written) as libc::size_t,
+                    (line.len() - written) as libc::c_uint,
                 )
             };
             if n <= 0 {
